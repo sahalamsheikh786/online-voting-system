f333b611e79db568b5f0e8a0fc7f22938450ff88
sahalamsheikh786-glitch <sahalamsheikh786@gmail.com>
2026-05-30 14:24:26 +0545

    Fix hosted CSS loading on Render

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/online-voting-system.css') }}">
+    <link rel="stylesheet" href="/css/online-voting-system.css?v={{ filemtime(public_path('css/online-voting-system.css')) }}">
     @stack('styles')
 </head>
 <body>
